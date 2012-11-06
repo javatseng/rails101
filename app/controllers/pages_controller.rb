345c27c6a16cb4c5bf2796b95897ec359db328b0
Stephen Tseng <javatseng@gmail.com>
2012-11-06 19:23:13 +0800

    Finished excercise 0

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+	def wolcome
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoForum::Application.config.secret_token = '8dd415c57ef74fcec9bc1c24b9c5e43cac2a6126efccf05b5d5a2ea469fac7cddee9e9d0a3d46fe60d890f8e868052d861dbdee134d3323738b1020ccc3bcaf0'
